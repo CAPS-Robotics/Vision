39789ac41063b8ab360b6719b5fd0f2d8895a1e8
ecjiang016 <95730592+ecjiang016@users.noreply.github.com>
2022-02-05 09:09:02 -0600

    Create profiler.py

---
+++ profiler.py
@@ -0,0 +1,23 @@
+import cProfile
+import io
+import pstats
+
+def profile(function):
+    """
+    A decorator that uses CProfile to profile a function
+    """
+
+    def inner(*args, **kwargs):
+        profiler = cProfile.Profile()
+        profiler.enable()
+        return_value = function(*args, **kwargs)
+        profiler.disable()
+        s = io.StringIO()
+        sortby = 'cumulative'
+        ps = pstats.Stats(profiler, stream=s).sort_stats(sortby)
+        ps.print_stats(50)
+        print(s.getvalue())
+        
+        return return_value
+
+    return inner
